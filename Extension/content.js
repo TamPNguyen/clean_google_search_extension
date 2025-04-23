43798e2d92eb3a16f944ad86cbabb7d0b407b985
Tam Phan-Nguyen <t477570@gmail.com>
2025-04-22 20:32:05 -0400

    Update content.js
    remove addtional whatPeopleAreSaying and addtional sponsorLinks

--- Extension/content.js
+++ Extension/content.js
@@ -24,9 +24,11 @@ document.addEventListener("DOMContentLoaded", () => {
         //https://www.google.com/search?q=laptop+14&sca_esv=fe1053e518bd0343&ei=sa_-Z_b7PObS5NoPm7-2-Ac&start=0&sa=N&sstk=Af40H4Xpy0D3bmse37l5CmrVVgfD2oyBEG3efFIvoFrR2TGN3H8Jna8AKkU7f9x6vlLPKn2Q9MR2ugmWuSpqjM8qOhd7bNjD3tJ60c6MW_AOP3oabwy2CL2SL_c22_03KlNx&ved=2ahUKEwi2mJej39qMAxVmKVkFHZufDX84FBDy0wN6BAgIEAQ&biw=1449&bih=1094&dpr=0.83
         if (mainURL.includes("search?q=")) {
             document.querySelectorAll(".c3mZkd.cu-container").forEach(sponsorLink => sponsorLink.remove()); //sponsorLinks
+            document.querySelectorAll(".uEierd").forEach(sponsorLink => sponsorLink.remove()); //sponsorLinks
             document.querySelectorAll("#Odp5De").forEach(mainSearch => mainSearch.remove()); //mainSearch
             document.querySelectorAll(".cUnQKe").forEach(peopleAlsoAsk => peopleAlsoAsk.remove()); //peopleAlsoAsk
             document.querySelectorAll(".ULSxyf.yhQgZb").forEach(WEPP => WEPP.remove()); //whatPeopleAreSaying exploreBrands peopleAlsoBuyFrom peopleAlsoSearchFor
+            document.querySelectorAll(".yG4QQe.TBC9ub.NbhJ1c").forEach(whatPeopleAreSaying => whatPeopleAreSaying.remove()); //whatPeopleAreSaying
             document.querySelectorAll(".NPn0Pb").forEach(fastPickupDelivery => fastPickupDelivery.remove()); //fastPickupDelivery
             document.querySelectorAll(".f8O9Qd.NPn0Pb").forEach(inStoreNearBy => inStoreNearBy.remove()); //inStoreNearBy
             document.querySelectorAll(".mnr-c.sPmWM").forEach(buyingGuild => buyingGuild.remove()); //buyingGuild
